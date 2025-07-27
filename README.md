ef0271f2f876618d627f293a8e768eed5096d7bd
booyaa <192864+booyaa@users.noreply.github.com>
2025-07-27 11:31:56 +0100

    refactor: handle non-meshtastic nodes serial connections

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 - [meshtastic_local_json_report.sh](./meshtastic_local_json_report.sh). See [HTTP API](https://meshtastic.org/docs/development/device/http-api/) doc and [firmware](https://github.com/meshtastic/firmware/blob/86af5f5252f408fce0fc1509e2430c98395c7d49/src/mesh/http/ContentHandler.cpp#L595) for more details.
 - [send_text_serial.py](./send_text_serial.py)
-- [send_text_serial_with_tcp_fallback.py](./send_text_serial_with_tcp_fallback.py)
+- [send_text_serial_with_tcp_fallback.py](./send_text_serial_with_tcp_fallback.py) - fairly robust way of trying serial and using tcp fallback. It will also handle when the serial isn't a meshtastic node.
 
 > [!TIP]
 > To set environment variable in PowerShell use `$env:TCP_HOSTNAME="1.2.3.4"`. For linux-ish use `export TCP_HOSTNAME="1.2.3.4"`.
--- send_text_serial_with_tcp_fallback.py
+++ send_text_serial_with_tcp_fallback.py
@@ -13,19 +13,24 @@ def onConnection(interface, topic=pub.AUTO_TOPIC):
 # https://python.meshtastic.org/#published-pubsub-topics
 pub.subscribe(onConnection, "meshtastic.connection.established")
 
-interface = meshtastic.serial_interface.SerialInterface()
+interface = None
 
-if interface.devPath is None:
+print("Attempting serial connection to Meshtastic node...")
+try:
+    interface = meshtastic.serial_interface.SerialInterface()
+except meshtastic.mesh_interface.MeshInterface.MeshInterfaceError as e:
+    print("Serial connection timed out or failed (preventing tcp fallback). Are you plugged into a Meshtastic device?")
+
+if interface is None or interface.devPath is None:
     print("Trying manual TCP connection fallback...")
     tcp_hostname = os.getenv('MESHTASTIC_HOST', 'meshtastic.local')
     try:
         interface = meshtastic.tcp_interface.TCPInterface(hostname=tcp_hostname)
+        print("Sleeping for 5 seconds......")
+        time.sleep(5)  # Allow some time for the connection to establish
     except Exception as e:
         print("Failed to connect to Meshtastic node using meshtastic.local.")
         print("Please set the MESHTASTIC_HOST environment variable to your Meshtastic node's IP address.")
         exit(1)
-
-time.sleep(5)  # Allow some time for the connection to establish
-print("Exiting...")
-# finally:
-interface.close()
+    finally:
+        interface.close()

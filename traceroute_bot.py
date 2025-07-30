f579a90730e10a908a1811d08dff60dbb6d6b550
Mark Sta Ana <mark.sta.ana@smartpension.co.uk>
2025-07-30 15:05:35 +0100

    feat: traceroute bot

---
+++ traceroute_bot.py
@@ -0,0 +1,74 @@
+import os
+from time import sleep
+import meshtastic
+import meshtastic.tcp_interface
+from pubsub import pub
+
+# event handlers
+def on_connection(interface, topic=pub.AUTO_TOPIC):
+    global my_node_info
+    my_node_info = interface.getMyNodeInfo()
+    battery_level = my_node_info.get('deviceMetrics',{}).get('batteryLevel', 'unknown')
+    long_name = my_node_info.get('user', {}).get('longName', 'Unknown')
+    id = my_node_info.get('user', {}).get('id', 'Unknown')
+    print(f"Connected to {long_name} (ID: {id}), Battery Level: {battery_level}%")
+    # import pprint
+    # pprint.pprint(my_node_info)
+
+def on_receive(packet, topic=pub.AUTO_TOPIC):
+    packet_type = packet.get('decoded', {}).get('portnum')
+    if packet_type == "TEXT_MESSAGE_APP":
+        message = packet.get('decoded', {}).get('payload', '')
+        from_node = packet.get('from', 'Unknown')
+        to = packet.get('to', 'Unknown') if packet.get('to') != meshtastic.BROADCAST_NUM else 'broadcast'
+
+        from_node_details = interface.nodesByNum[from_node]
+        from_long_name = from_node_details.get('user', {}).get('longName', 'Unknown')
+        from_short_name = from_node_details.get('user', {}).get('shortName', 'Unknown')
+        from_id = from_node_details.get('user', {}).get('id', 'Unknown')
+        from_hw = from_node_details.get('user', {}).get('hwModel', 'Unknown')
+        print(f"Sender details: {from_long_name} ({from_short_name}) ID: {from_id} Hardware: {from_hw}")
+        if to == 'broadcast':
+            channel = packet.get('channel', 'DEFAULT')
+            print(f"Received: {message} from {from_node} on channel {channel}")
+        else:
+            print(f"Received: {message} (DM) from {from_node} to {'me' if to == my_node_info['num'] else to}")
+            # FIXME: need to get this data out of this method
+            interface.sendTraceRoute(dest=from_node, hopLimit=5)
+            print("-----")
+
+
+
+
+# events
+pub.subscribe(on_connection, "meshtastic.connection.established")
+pub.subscribe(on_receive, "meshtastic.receive")
+
+def main():
+    tcp_hostname = os.getenv('MESHTASTIC_HOST', 'meshtastic.local')
+    try:
+        global interface
+        interface = meshtastic.tcp_interface.TCPInterface(hostname=tcp_hostname)
+        while True:
+            sleep(1)
+            try:
+                interface.sendHeartbeat()  # Keep the connection alive
+            except interface.MeshInterfaceError as e:
+                print(f"Heartbeat failed: {e}")
+                # FIXME: try to reconnect
+                break
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received. Exiting...")
+    except Exception as e:
+        print("Failed to connect to Meshtastic node using meshtastic.local.")
+        print("Please set the MESHTASTIC_HOST environment variable to your Meshtastic node's IP address.")
+        exit(1) # no interface to close
+    finally:
+        print("we come to the end...")
+        interface.close()
+
+
+
+if __name__ == "__main__":
+    print("Starting Meshtastic TCP client...")
+    main()

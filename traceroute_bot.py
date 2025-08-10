c24632434f9b6e297b6a61b8f96016f63b51d61f
booyaa <192864+booyaa@users.noreply.github.com>
2025-08-10 17:16:51 +0100

    refactor: improve traceroute logging and command response

--- traceroute_bot.py
+++ traceroute_bot.py
@@ -1,7 +1,9 @@
 import contextlib
 import os
+from platform import node
 import pprint
 from time import sleep
+import time
 import meshtastic
 import meshtastic.tcp_interface
 from pubsub import pub
@@ -9,8 +11,12 @@ import logging
 from logging import handlers
 import contextlib
 import io
+import math
 
 interface = None
+# initialise last run 30 seconds ago to avoid rate limiting on start up
+last_run = time.localtime(time.time() - 30)
+our_coords = None  # (latitude, longitude)
 
 logging.basicConfig(
     # Usage: TRACEROUTE_BOT_LOG_LEVEL=debug python traceroute_bot.py
@@ -32,10 +38,26 @@ def on_connection(interface, topic=pub.AUTO_TOPIC):
     long_name = my_node_info.get('user', {}).get('longName', 'Unknown')
     id = my_node_info.get('user', {}).get('id', 'Unknown')
     logging.info(f"Connected to {long_name} (ID: {id}), Battery Level: {battery_level}%")
-    # no channel details
-    # pprint.pprint(interface.showInfo())
-    # full channel details, but how do you parse it?
-    # pprint.pprint(interface.getNode(id).showInfo())
+
+    node = interface.getNode('^local')
+    global our_coords
+    if our_coords is None:
+        node_info = interface.getMyNodeInfo()
+        our_coords = (node_info.get('position', {}).get('latitude', {}), node_info.get('position', {}).get('longitude', {}))
+        logging.warning(f"No coordinates set using TRACEBOT_COORDS, using default from device: {our_coords}")
+    channels = node.channels
+    channel_log = ""
+    if channels:
+        channel_log = "Listening on channels:"
+        for channel in channels:
+            if channel.role:
+                channel_log += f" {"default" if len(channel.settings.name) == 0 else channel.settings.name} (ch: {channel.index}) - {"primary" if channel.role == 1 else "secondary"} /"
+        channel_log = channel_log[:-2] # trim trailing separator
+    else:
+        channel_log = "No channels found."
+
+    logging.info(channel_log)
+
 
 def on_receive(packet, topic=pub.AUTO_TOPIC):
     packet_type = packet.get('decoded', {}).get('portnum')
@@ -45,17 +67,38 @@ def on_receive(packet, topic=pub.AUTO_TOPIC):
         from_node = packet.get('from', 'Unknown')
         to = packet.get('to', 'Unknown') if packet.get('to') != meshtastic.BROADCAST_NUM else 'broadcast'
         from_node_details = get_node_info(from_node)
-        logging.info(f"Sender details: {from_node_details["string"]}")
-        if to == 'broadcast':
-            channel = packet.get('channel', 'DEFAULT')
-            if not message.decode().lower().startswith('traceroute'):
-                logging.debug(f"Received: {message} from {from_node} on channel {channel}")
-                return
-            logging.info(f"Received traceroute ({message}) request from {from_node} on channel {channel}, sending trace route")
-            lazy_traceroute(from_node)
+        if not message.decode().lower().startswith('traceroute'):
+           return  # Ignore non-traceroute messages
+        
+        message = message.decode().lower().replace("traceroute", "").strip()
+        parts = message.split(",")
+        if len(parts) == 3:
+            latitude = float(parts[0].strip())
+            longitude = float(parts[1].strip())
+            label = parts[2].strip() + " - "or ""
         else:
-            logging.info(f"Received: {message} (DM) from {from_node} to {'me' if to == my_node_info['num'] else to}, sending trace route")
-            lazy_traceroute(from_node)
+            latitude = longitude = None
+            label = message + " - " if len(message) > 0 else ""
+            print("DEBUG", label)
+        
+        # add cool off period
+        current_time = time.localtime()
+        elapsed_seconds = (current_time.tm_min * 60 + current_time.tm_sec) - (last_run.tm_min * 60 + last_run.tm_sec)
+        logging.info(f"Elapsed seconds since last traceroute: {elapsed_seconds}")
+        if elapsed_seconds <= 30: # ios
+            logging.warning(f"Skipping traceroute due to rate limiting, elapsed seconds: {elapsed_seconds}")
+            traceroute = "skipped"
+        else:
+            traceroute = lazy_traceroute(from_node)
+        
+        global our_coords
+        destination_coords = from_node_details['coords'] if latitude is None or longitude is None else (latitude, longitude)
+        if latitude is None or longitude is None:
+            logging.warning(f"Using device coordinates: {destination_coords} for distance measuring. This maybe inaccurate unless channel is using precision.")
+
+        print(f"DEBUG|Coords: {destination_coords}, Label: {label}, Elapsed seconds: {elapsed_seconds}")
+        distance_to_destination = distance(our_coords, destination_coords)
+        logging.info(f"{label}{traceroute} - distance to destination: {distance_to_destination:.2f} km")
     else:
         logging.debug(f"Received packet of type {packet_type}")
 
@@ -70,26 +113,79 @@ def get_node_info(node_num):
     """Retrieve node information by node number."""
     node_info = interface.nodesByNum.get(node_num, {})
     user_info = node_info.get('user', {})
+    coords = (node_info.get('position', {}).get('latitude', {}), node_info.get('position', {}).get('longitude', {}))
     return {
         'longName': user_info.get('longName', 'Unknown'),
         'shortName': user_info.get('shortName', 'Unknown'),
         'id': user_info.get('id', 'Unknown'),
         'hwModel': user_info.get('hwModel', 'Unknown'),
-        'string': f"{user_info.get('longName', 'Unknown')} ({user_info.get('shortName', 'Unknown')}) ID: {user_info.get('id', 'Unknown')} Hardware: {user_info.get('hwModel', 'Unknown')}"
+        'coords': coords,
+        'string': f"{user_info.get('longName', 'Unknown')} ({user_info.get('shortName', 'Unknown')}) ID: {user_info.get('id', 'Unknown')} Hardware: {user_info.get('hwModel', 'Unknown')} Coords: {coords}"
     }
 
 def lazy_traceroute(from_node):
+    message = None
     try:
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
             interface.sendTraceRoute(dest=from_node, hopLimit=5)
-        output = f.getvalue().replace('\n', ' ')
-        logging.info(output)
-        interface.sendText(text=output, destinationId=from_node)
+        message = f.getvalue().replace('\n', ' ')
+        global last_run
+        last_run = time.localtime()
     except interface.MeshInterfaceError as e:
-        logging.error(f"Failed to send traceroute: {e}")
+        message = f"Failed to send traceroute: {e}"
+        logging.error(message)
+
+    return message
+
+def distance(origin, destination):
+    """
+    Source: https://stackoverflow.com/a/38187562
+    Calculate the Haversine distance.
+
+    Parameters
+    ----------
+    origin : tuple of float
+        (lat, long)
+    destination : tuple of float
+        (lat, long)
+
+    Returns
+    -------
+    distance_in_km : float
+
+    Examples
+    --------
+    >>> origin = (48.1372, 11.5756)  # Munich
+    >>> destination = (52.5186, 13.4083)  # Berlin
+    >>> round(distance(origin, destination), 1)
+    504.2
+    """
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371  # km
+
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
+         math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
+         math.sin(dlon / 2) * math.sin(dlon / 2))
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = radius * c
+
+    return d
 
 def main():
+    global our_coords
+    tracebot_env_coords = os.getenv("TRACEBOT_COORDS", None)
+    if tracebot_env_coords:
+        try:
+            latitude, longitude = map(float, tracebot_env_coords.split(","))
+            our_coords = (latitude, longitude)
+            logging.info(f"Using coordinates from TRACEBOT_COORDS: {our_coords}")
+        except ValueError:
+            logging.error(f"Invalid TRACEBOT_COORDS format: {tracebot_env_coords}")
+
     tcp_hostname = os.getenv('MESHTASTIC_HOST', 'meshtastic.local')
     try:
         global interface

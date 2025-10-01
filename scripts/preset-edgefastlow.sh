bb6a7b65e3f7a86d93da3aa1ab35d09ca3d1c817
booyaa <192864+booyaa@users.noreply.github.com>
2025-10-01 20:34:28 +0100

    refactor: add missing parameters

--- scripts/preset-edgefastlow.sh
+++ scripts/preset-edgefastlow.sh
@@ -8,10 +8,14 @@ if [ -z "$tcp_host" ]; then
 fi
 
 uvx meshtastic --host "$tcp_host" \
-    --set lora.modem_preset LONG_FAST \
-    --set lora.bandwidth 0 \
-    --set lora.spread_factor 0 \
-    --set lora.coding_rate 0 \
-    --set lora.frequency_offset 0.0
+    --set lora.use_preset false \
+    --set lora.bandwidth 62 \
+    --set lora.spread_factor 8 \
+    --set lora.coding_rate 8 \
+    --set lora.frequency_offset 0.0 \
+    --set lora.channel_num 1 \
+    --set lora.override_frequency 869.4313 \
+    --ch-index 0 \
+    --ch-set name "EdgeFastLow"
 
 echo "Warning: you need to manually fix the channel settings"
